ddf40dc7e40db874b91796faf4494b8096d6db8a
romulofff <romulofffufc@gmail.com>
2020-10-29 18:11:50 -0300

    Creating folder for artificial charts. Creating a Utils.py file, to stop rewriting code.

--- src/tutorial.py
+++ src/tutorial.py
@@ -1,12 +1,14 @@
 # Tutorial from Reddit
 # https://www.reddit.com/r/pygame/comments/9z4kpq/how_to_create_a_rhythm_game/
-
+import os 
 import random
 from time import time
 
 import pygame
-from pygame import draw
 from numpy import arange
+from pygame import draw
+
+from utils import draw_line, draw_note_target
 
 # pygame.mixer.pre_init(frequency=22050, size=-16, channels=2, buffersize=512)
 pygame.init()
@@ -18,7 +20,7 @@ when_things_happen = [x for x in arange(0, 10, 0.5)]
 # print(when_things_happen, len(when_things_happen))
 start_time = time()
 
-interval = 10.0
+interval = 1.0
 def check_if_close(elapsed_time, times):
     for time in times:
         if elapsed_time - interval <= time <= elapsed_time + interval:
@@ -30,37 +32,22 @@ def check_if_close(elapsed_time, times):
 
     return False
 
-def draw_note_target():
-    """Draws the note target at the end of the track"""
-    target_y = 660
-    target_outer_radius = 30
-    target_inner_radius = 15
-
-    gray = (55, 55, 55)
-
-    # Outer (colored) Circle
-    draw.circle(screen, (0, 255, 0), (64, target_y), target_outer_radius)
-    draw.circle(screen, (255, 0, 0), (132, target_y), target_outer_radius)
-    draw.circle(screen, (255, 255, 0), (200, target_y), target_outer_radius)
-    draw.circle(screen, (0, 0, 255), (268, target_y), target_outer_radius)
-    draw.circle(screen, (255, 128, 0), (336, target_y), target_outer_radius)
-    # Inner Circle
-    draw.circle(screen, gray, (64, target_y), target_inner_radius)
-    draw.circle(screen, gray, (132, target_y), target_inner_radius)
-    draw.circle(screen, gray, (200, target_y), target_inner_radius)
-    draw.circle(screen, gray, (268, target_y), target_inner_radius)
-    draw.circle(screen, gray, (336, target_y), target_inner_radius)
-
 if __name__ == "__main__":
+
+    # NÃO VAI FUNCIONAR COM ESSA IMAGEM
+    green_note = pygame.image.load(os.path.join('..', 'green_note.png')).convert()
+
+
     running = True
     font_name = pygame.font.match_font('arial')
     font = pygame.font.Font(font_name, 50)
+    y = 0
     while running:
         color = (255,255,255)
         if len(when_things_happen) == 0: running = False
         elapsed_time = time() - start_time
         happening = [x for x in when_things_happen if x <= elapsed_time]
-        print(happening)
+        # print(happening)
 
         for e in pygame.event.get():
             if e.type==pygame.KEYDOWN and e.key == 27:
@@ -78,6 +65,7 @@ if __name__ == "__main__":
             text_surface = font.render(str(x), True, color)
             screen.blit(text_surface, (180,140))
             when_things_happen.remove(x)
-        draw_note_target()
+        draw_line(screen)
+        draw_note_target(screen)
         pygame.display.update()
-        clock.tick(10)
+        clock.tick(60)
---
+++ src/utils.py
@@ -0,0 +1,33 @@
+from pygame import draw
+
+
+def draw_line(screen):
+    """ Draw the lines where the notes will roll """
+    height = screen.get_height()
+    draw.line(screen, (100, 100, 100), (120, 0), (40, height), 3)
+    draw.line(screen, (100, 100, 100), (160, 0), (120, height), 3)
+    draw.line(screen, (100, 100, 100), (200, 0), (200, height), 3)
+    draw.line(screen, (100, 100, 100), (240, 0), (280, height), 3)
+    draw.line(screen, (100, 100, 100), (280, 0), (360, height), 3)
+
+def draw_note_target(screen):
+    """Draws the note target at the end of the track"""
+    height = screen.get_height()
+    target_y = int(height - 10/100*height)
+    target_outer_radius = 30
+    target_inner_radius = 15
+
+    gray = (55, 55, 55)
+
+    # Outer (colored) Circle
+    draw.circle(screen, (0, 255, 0), (64, target_y), target_outer_radius)
+    draw.circle(screen, (255, 0, 0), (132, target_y), target_outer_radius)
+    draw.circle(screen, (255, 255, 0), (200, target_y), target_outer_radius)
+    draw.circle(screen, (0, 0, 255), (268, target_y), target_outer_radius)
+    draw.circle(screen, (255, 128, 0), (336, target_y), target_outer_radius)
+    # Inner Circle
+    draw.circle(screen, gray, (64, target_y), target_inner_radius)
+    draw.circle(screen, gray, (132, target_y), target_inner_radius)
+    draw.circle(screen, gray, (200, target_y), target_inner_radius)
+    draw.circle(screen, gray, (268, target_y), target_inner_radius)
+    draw.circle(screen, gray, (336, target_y), target_inner_radius)
