2234cd9a92160a85d360c520a26487940cecd9e5
romulofff <romulofffufc@gmail.com>
2020-11-14 12:53:39 -0300

    Ainda não funciona

--- src/tutorial.py
+++ src/tutorial.py
@@ -10,32 +10,31 @@ from pygame import draw
 
 from utils import draw_line, draw_note_target
 
-# pygame.mixer.pre_init(frequency=22050, size=-16, channels=2, buffersize=512)
-pygame.init()
-clock = pygame.time.Clock()
-
-screen = pygame.display.set_mode((450, 720))
-
-when_things_happen = [x for x in arange(0, 10, 0.5)]
 # print(when_things_happen, len(when_things_happen))
 start_time = time()
 
 interval = 1.0
 def check_if_close(elapsed_time, times):
+    print("TIMES:",times)
     for time in times:
+        print("TIME:", time)
         if elapsed_time - interval <= time <= elapsed_time + interval:
         # if time - interval <= elapsed_time <= time + interval:
         
            print(elapsed_time, time+interval, time-interval, time, True)
            return True
-        print(elapsed_time, time+interval, time-interval, time)
+        print(elapsed_time, time+interval, time-interval, time, False)
 
     return False
 
-if __name__ == "__main__":
+def Game():
+    # pygame.mixer.pre_init(frequency=22050, size=-16, channels=2, buffersize=512)
+    pygame.init()
+    clock = pygame.time.Clock()
 
-    # NÃO VAI FUNCIONAR COM ESSA IMAGEM
-    green_note = pygame.image.load(os.path.join('..', 'green_note.png')).convert()
+    screen = pygame.display.set_mode((450, 720))
+
+    when_things_happen = [x for x in arange(0, 10, 0.5)]
 
 
     running = True
@@ -43,6 +42,7 @@ if __name__ == "__main__":
     font = pygame.font.Font(font_name, 50)
     y = 0
     while running:
+        print("MAIN LOOP START")
         color = (255,255,255)
         if len(when_things_happen) == 0: running = False
         elapsed_time = time() - start_time
@@ -51,15 +51,15 @@ if __name__ == "__main__":
 
         for e in pygame.event.get():
             print("Got event!", e)
-            if e.type==pygame.KEYDOWN and e.key == 27:
-                print("Bye!")
-                quit()
-            if e.type==pygame.KEYDOWN and e.key == 32:
-            # if e.type==pygame.KEYDOWN:
-                if check_if_close(elapsed_time, happening):
-                    print("SPACE BAR PRESSED RIGHT")
-                    color = (255,0,0)
-                else: color = (255,255,255)
+            if e.type==pygame.KEYDOWN:
+                if e.key == 27:
+                    print("Bye!")
+                    quit()
+                if e.key == 32 or e.key == pygame.K_a:
+                    if check_if_close(elapsed_time, happening):
+                        print("SPACE BAR PRESSED RIGHT")
+                        color = (255,0,0)
+                    else: color = (255,255,255)
 
         for x in happening:
             screen.fill(0)
@@ -69,4 +69,4 @@ if __name__ == "__main__":
         draw_line(screen)
         draw_note_target(screen)
         pygame.display.update()
-        clock.tick(60)
+        clock.tick(30)
