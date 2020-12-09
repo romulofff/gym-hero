1e9086c677c7db53b1208f0f10e0557009b61e5b
romulofff <romulofffufc@gmail.com>
2020-12-09 09:46:49 -0300

    Colliding notes and frets

--- src/game.py
+++ src/game.py
@@ -51,7 +51,7 @@ class Fret(pygame.sprite.Sprite):
             self.rect.x = 400
         if color.lower() == 'blue':
             self.rect.x = 500
-        
+
 
 pygame.init()
 screen_width = 800
@@ -67,15 +67,29 @@ redY = 100  # randomizes intial y-position
 redChange = random.uniform(2, 3)  # randomizes intial speed
 red_note = Note(redImg, redX, redY)
 
-note_list = pygame.sprite.Group()
+green_notes_list = pygame.sprite.Group()
+red_notes_list = pygame.sprite.Group()
+yellow_notes_list = pygame.sprite.Group()
+blue_notes_list = pygame.sprite.Group()
+all_notes_list = pygame.sprite.Group()
 all_sprites_list = pygame.sprite.Group()
 
 for i in range(50):
     note = Note(redImg, redX, redY)
-    note.rect.x = random.choice([200, 300, 400, 500])
+    rect_x = random.choice([200, 300, 400, 500])  # choose note color
+    note.rect.x = rect_x
     note.rect.y = random.uniform(-600*3, -60)
 
-    note_list.add(note)
+    if rect_x == 200:
+        green_notes_list.add(note)
+    if rect_x == 300:
+        red_notes_list.add(note)
+    if rect_x == 400:
+        yellow_notes_list.add(note)
+    if rect_x == 500:
+        blue_notes_list.add(note)
+
+    all_notes_list.add(note)
     all_sprites_list.add(note)
 
 # GreenFret
@@ -107,22 +121,31 @@ clock = pygame.time.Clock()
 score = 0
 done = False
 while not done:
+    green_notes_hit_list = pygame.sprite.spritecollide(
+        greenFret, green_notes_list, False)
+    red_notes_hit_list = pygame.sprite.spritecollide(
+        redFret, red_notes_list, False)
+    yellow_notes_hit_list = pygame.sprite.spritecollide(
+        yellowFret, yellow_notes_list, False)
+    blue_notes_hit_list = pygame.sprite.spritecollide(
+        blueFret, blue_notes_list, False)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
         # if event.type == pygame.KEYDOWN:
-        #     if event.key == pygame.K_DOWN:
-        #         redFret.rect.x += 10
+            if event.key == pygame.K_a:
+                redFret.rect.x += 10
         #     if event.key == pygame.K_UP:
         #         redFret.rect.y += 10
 
     screen.fill((0, 0, 0))
 
-    notes_hit_list = pygame.sprite.spritecollide(redFret, note_list, True)
-    if len(notes_hit_list) > 0:
-        print(notes_hit_list)
-    note_list.update()
+    # Move notes down
+    all_notes_list.update()
+
     all_sprites_list.draw(screen)
+
     clock.tick(60)
     pygame.display.flip()
 
